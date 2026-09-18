90b0d86b316c29927dc045ed09dc05f9ea5d4e39
gabriellimaavila <gabiavila08@gmail.com>
2026-09-18 17:02:37 -0300

    feat: Adiciona component imagesCard

--- front-end/src/modules/detalhes-objeto/pages/detalhesObjeto.jsx
+++ front-end/src/modules/detalhes-objeto/pages/detalhesObjeto.jsx
@@ -1,13 +1,55 @@
+import { useParams, NavLink } from 'react-router'; 
 
+import './detalhesObjeto.css'; 
 
+import objetos from '../../../modules/buscar-objetos/components/principalCards/objetos.json'; 
+
+import houseGray from '../../../assets/icons/detalhesObjeto/house-gray.svg'; 
+import arrowGray from '../../../assets/icons/detalhesObjeto/arrow-gray.svg'; 
+import arrowBlue from '../../../assets/icons/detalhesObjeto/arrow-blue.svg'; 
+
+import ImagesCard from '../components/imagesCard/ImagesCard.jsx'; 
 
 function detalhesObjeto(){
 
+    const params = useParams(); 
+
+    const objetoEncontrado = objetos.filter((objetos) => { if (objetos.id === Number(params.id)) { return objetos }} )
 
     return (
-        <div>
-            <p>estou aqui</p>
-        </div>
+        <section className='detalhe__objetos'>
+
+                <div className='caminho__voltar'>
+
+                        <div className='caminho'>
+                                <img className='house__gray' src={houseGray} />
+                                <img src={arrowGray} />
+                                <p className='caminho__text'>Buscar objetos / {objetoEncontrado[0].nome}</p>
+                        </div>
+
+                        <div className='voltar__buscar'>
+
+                            <NavLink className='voltar__navlink' to='/buscar-objetos' end>
+                                <img src={arrowBlue} />
+                                <p className='voltar__text'> voltar para Buscar Objetos </p>
+                            </NavLink>
+                                
+                        </div>
+                </div>
+
+                <div className='container__principal'>
+
+                    <div className='esquerda'>
+                        <ImagesCard objeto={objetoEncontrado} />
+                    </div>
+
+                    <div className='direita'>
+                        
+                    </div>
+
+                </div>
+
+        </section>
     )
 }
 
